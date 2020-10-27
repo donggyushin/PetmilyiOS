03fa35c53ad7611f2d744dc8ca6a67ef943a1a8e
shin donggyu <donggyu9410@gmail.com>
2020-10-27 22:18:23 +0900

    정규식 수정

--- Petmily-ios/Properties/Properties.swift
+++ Petmily-ios/Properties/Properties.swift
@@ -10,10 +10,10 @@ import Foundation
 class Properties {
     public static var token:String?
     public static var PETMILY_API = "http://210.123.254.17:9042/api"
-    // 최소 한개의 문자와 1개의 숫자 그리고 한개의 특수문자로 이루어진 8자 이상, 20자 이하
-    public static let passwordRegex = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@$!%*#?&])[A-Za-z\\d$@$!%*#?&]{8,20}$"
+    // 최소 1개의 숫자 혹은 특수 문자를 포함해야 함, 8자 이상 20자 이하
+    public static let passwordRegex = "^(?=.*[a-zA-Z])((?=.*\d)|(?=.*\W)).{8,20}$"
     // 한글로 2자이상 5자이하
     public static let nicknameRegex = "^[가-힣//s]{2,5}$"
-    // 최소 한개 이상의 문자와 길이는 8자 이상 20자 이하
-    public static let userIdRegex = "(?i)^(?=.*[a-z])[a-z0-9]{8,20}$"
+    // 특수문자 사용 불가 8자 이상 20자 이하
+    public static let userIdRegex = "^[a-zA-Z0-9]{8,20}$"
 }
