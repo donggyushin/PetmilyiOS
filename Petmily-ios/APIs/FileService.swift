8316285cb661ffeac48b0b03e631c1773d29da2e
shin donggyu <donggyu9410@gmail.com>
2020-11-09 23:13:27 +0900

    여러 이미지 동기로 업로드하는 uploadImageFiles 유틸 함수 생성

--- Petmily-ios/APIs/FileService.swift
+++ Petmily-ios/APIs/FileService.swift
@@ -14,6 +14,49 @@ class FileService {
     let storage = Storage.storage()
     let imageRef = Storage.storage().reference().child("images")
     
+    func uploadImageFiles(images:[UIImage], completion:@escaping(Error?, String?, [String]?) -> Void){
+        var imageDatas:[Data] = []
+        var urls:[String] = []
+        let group = DispatchGroup()
+        
+        
+        images.forEach { (image) in
+            guard let data = image.pngData() else { return completion(nil, "데이터로 변환하던 도중에 손실된 이미지가 있습니다.", nil)}
+            imageDatas.append(data)
+        }
+        
+        if imageDatas.count != images.count {
+            return completion(nil, "데이터로 변환하던 도중에 손실된 이미지가 있습니다.", nil)
+        }
+        
+        
+        imageDatas.forEach { (data) in
+            
+            let uuid = NSUUID().uuidString
+            let imageRef = self.imageRef.child("\(uuid).jpg")
+            group.enter()
+            let uploadTask = imageRef.putData(data, metadata: nil) { (metadata, error) in
+                if let error = error {
+                    return completion(error, nil, nil)
+                }
+                imageRef.downloadURL { (url, error) in
+                    if let error = error {
+                        return completion(error, nil, nil)
+                    }
+                    guard let url = url else { return }
+                    urls.append(url.absoluteString)
+                    group.leave()
+                    
+                }
+            }
+            uploadTask.resume()
+        }
+        
+        group.notify(queue: DispatchQueue.main) {
+            return completion(nil, nil, urls)
+        }
+    }
+    
     func uploadImageFile(image:UIImage, completion:@escaping(Error?, String?, String?) -> Void) {
         guard let data = image.pngData() else {
             completion(nil, "이미지를 데이터로 변환하던 도중에 에러가 발생하였습니다", nil)
